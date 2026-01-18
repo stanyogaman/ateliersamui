44a8bce05f54a6e5d892c3fe98a3366705941dea
stanyogaman <stanyogaman@gmail.com>
2026-01-18 11:40:44 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   i18n: {
     locales: ['en', 'ru', 'th', 'he'],
     defaultLocale: 'en',
+    localeDetection: true,
+  },
+  images: {
+    domains: ['atelier-samui.com'], // ЗАМЕНИТЕ на ваш домен Cloudflare R2
   },
 }
+
+module.exports = nextConfig
