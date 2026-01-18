44948cf1d846c40d3c1ba46639012b7c5988199a
stanyogaman <stanyogaman@gmail.com>
2026-01-18 11:41:35 +0700

    Create ru.js

---
+++ locales/ru.js
@@ -0,0 +1,7 @@
+export const ru = {
+  title: "Atelier Samui",
+  heroH1: "Эстетика Пальмового Дерева",
+  heroP: "Минималистичные решения для вилл и отелей.",
+  calc: "Калькулятор",
+  dir: "ltr"
+};
